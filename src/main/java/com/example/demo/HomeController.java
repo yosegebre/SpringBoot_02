138036b1c8a5d6eb1569e38faac73f745aeca676
GBTC406008UR <GBTC406008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-13 17:01:57 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homepage(Model model){
+        model.addAttribute("myvar", "say hello world to the people");
+        return "hometemplate";
+    }
+}
